3215bc0a9fd985b5a69de25bd0e2cff48eff64de
Muhammad Alful Falakh <alfulfal@gmail.com>
2021-05-05 18:23:04 +0700

    masih error bagian admin dashboard
    error mungkin gra" bug

---
+++ CatalogBoardGame/app/src/main/java/com/example/catalogboardgame/DashboardAdm.java
@@ -0,0 +1,21 @@
+package com.example.catalogboardgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class DashboardAdm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_adm);
+    }
+
+    public void daftargame(View view) {
+    }
+
+    public void history(View view) {
+    }
+}
--- CatalogBoardGame/app/src/main/java/com/example/catalogboardgame/DashboardAdmin/DashboardAdmin.java
+++ CatalogBoardGame/app/src/main/java/com/example/catalogboardgame/DashboardAdmin/DashboardAdmin.java
@@ -2,8 +2,11 @@ package com.example.catalogboardgame.DashboardAdmin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.catalogboardgame.DaftarGameClient.DaftarGameClientActivity;
 import com.example.catalogboardgame.R;
 
 public class DashboardAdmin extends AppCompatActivity {
@@ -13,4 +16,14 @@ public class DashboardAdmin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_admin);
     }
+
+//    public void DAFTARGAME(View view) {
+//        Intent intent=new Intent(DashboardAdmin.this, DaftarGameClientActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    public void Historyadmin(View view) {
+//        Intent intent=new Intent(DashboardAdmin.this, DaftarGameClientActivity.class);
+//        startActivity(intent);
+//    }
 }
