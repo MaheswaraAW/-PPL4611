5fcd3f783221d962005d2ee1aa0c10a251f038e8
Muhammad Alful Falakh <alfulfal@gmail.com>
2021-05-10 13:54:35 +0700

    log in sign up masih belum pake firebase authentification

--- CatalogBoardGame/app/src/main/java/com/example/catalogboardgame/DashboardAdmin/dbAdmin.java
+++ CatalogBoardGame/app/src/main/java/com/example/catalogboardgame/DashboardAdmin/dbAdmin.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.catalogboardgame.DaftarGameClient.DaftarGameClientActivity;
 import com.example.catalogboardgame.R;
+import com.example.catalogboardgame.Sign.SignUp;
 
 public class dbAdmin extends AppCompatActivity {
 
@@ -15,6 +17,8 @@ public class dbAdmin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_db_admin);
+
+
     }
 
     public void DAFTARGAME(View view) {
--- CatalogBoardGame/app/src/main/java/com/example/catalogboardgame/Sign/SignIn.java
+++ CatalogBoardGame/app/src/main/java/com/example/catalogboardgame/Sign/SignIn.java
@@ -2,15 +2,41 @@ package com.example.catalogboardgame.Sign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.catalogboardgame.DaftarGameClient.DaftarGameClientActivity;
+import com.example.catalogboardgame.DashboardAdmin.dbAdmin;
 import com.example.catalogboardgame.R;
 
 public class SignIn extends AppCompatActivity {
 
+    Button ss,sd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        ss=findViewById(R.id.ss);
+        sd=findViewById(R.id.sd);
+
+        ss.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(SignIn.this, dbAdmin.class);
+                startActivity(intent);
+
+            }
+        });
     }
+//
+//    public void Login(View view) {
+//    }
+//
+//    public void Register(View view) {
+//        Intent intent = new Intent(SignIn.this,SignUp.class);
+//        startActivity(intent);
+//    }
 }
