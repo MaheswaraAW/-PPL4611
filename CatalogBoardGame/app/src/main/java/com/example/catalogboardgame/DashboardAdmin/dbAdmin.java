2529e49f6b4beb64c2c1501cc69cd82b4dc1e8fa
Muhammad Alful Falakh <alfulfal@gmail.com>
2021-05-20 10:05:17 +0700

    a

--- CatalogBoardGame/app/src/main/java/com/example/catalogboardgame/DashboardAdmin/dbAdmin.java
+++ CatalogBoardGame/app/src/main/java/com/example/catalogboardgame/DashboardAdmin/dbAdmin.java
@@ -5,11 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import com.example.catalogboardgame.DaftarGameClient.DaftarGameClientActivity;
 import com.example.catalogboardgame.R;
-import com.example.catalogboardgame.Sign.SignUp;
 
 public class dbAdmin extends AppCompatActivity {
 
@@ -17,8 +15,6 @@ public class dbAdmin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_db_admin);
-
-
     }
 
     public void DAFTARGAME(View view) {
--- CatalogBoardGame/app/src/main/java/com/example/catalogboardgame/Sign/SignUp.java
+++ CatalogBoardGame/app/src/main/java/com/example/catalogboardgame/Sign/SignUp.java
@@ -1,85 +1,16 @@
 package com.example.catalogboardgame.Sign;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.example.catalogboardgame.DaftarGameClient.DaftarGameClientActivity;
-import com.example.catalogboardgame.DashboardAdmin.dbAdmin;
 import com.example.catalogboardgame.R;
 
-import com.example.catalogboardgame.model.Akun;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 public class SignUp extends AppCompatActivity {
-
-    Button sin;
-    Button regis;
-    EditText email, name,password;
-    Akun akun;
-    DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference("Akun");
-    String ad="1";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
-         sin= findViewById(R.id.logs);
-         regis= findViewById(R.id.sa);
-
-//        email=findViewById(R.id.regemailuser);
-//        name =findViewById(R.id.regnamauser);
-//        password=findViewById(R.id.regpassworduser);
-
-        regis.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(SignUp.this, dbAdmin.class);
-                startActivity(intent);
-
-            }
-        });
-//        register.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-////                        akun.setNama(name.getText().toString());
-////                        akun.setEmail(email.getText().toString());
-////                        akun.setPassword(password.getText().toString());
-////                        akun.setManag(ad);
-////                        databaseReference.push().setValue(akun);
-//                Toast.makeText(SignUp.this, "Data Tersimpan", Toast.LENGTH_SHORT).show();
-//
-//
-//            }
-//        });
-//        sign.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText(SignUp.this, "Data Tersimpan", Toast.LENGTH_SHORT).show();
-//
-//                Intent intent = new Intent(SignUp.this,SignIn.class);
-//                startActivity(intent);
-//            }
-//        });
     }
-//
-//    public void login(View view) {
-//        Intent intent = new Intent(SignUp.this,dbAdmin.class);
-//        startActivity(intent);
-//
-//    }
-//
-//    public void register(View view) {
-//    }
 }
